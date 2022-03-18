f8013c64b89103a12e953e5d138f16ab016aa999
NoaJel <noa.jelsch@viacesi.fr>
2022-03-18 21:39:42 +0100

    Header styles done

--- head.php
+++ head.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="./scss/common.css" />
-    <link rel="stylesheet" href="./scss/header.css" type="text/css" />
+    <!-- <link rel="stylesheet" href="./scss/common.css" /> -->
+    <link rel="stylesheet" href="./scss/header-stu.css" type="text/css" />
+    <link rel="stylesheet" href="./scss/header-adm.css" type="text/css" />
     <link rel="stylesheet" href="./scss/footer.css" type="text/css" />
     <!-- FONTS -->
     <link rel="stylesheet" href="./fonts/fonts.css" />
@@ -14,3 +15,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500;700&display=swap" rel="stylesheet" />
     <!-- /FONTS -->
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="./fontawesome-free-5.15.4-web/css/all.min.css" />
+    <!-- /FONTAWESOME -->
---
+++ header-stu.php
@@ -0,0 +1,16 @@
+<header>
+    <a href="/"><img src="./images/logo.png" alt="uLink logo" class="ulink-logo" /></a>
+    <nav>
+        <ul class="nav-links">
+            <li><a href="#">Your internships</a></li>
+            <li><a href="#">Wishlist</a></li>
+            <li><a href="#">Research &nbsp; <i class="fas fa-search"></i></a></li>
+        </ul>
+    </nav>
+    <div class="nav-icons">
+        <i class="fas fa-bell"></i>
+        <button type="button" class="login-btn">
+            Login <i class="fas fa-user-alt"></i>
+        </button>
+    </div>
+</header>
