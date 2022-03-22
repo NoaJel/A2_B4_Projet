431249e34d4e1f3b13400e610baacf6a4ad0990b
SwenA <swen.aubertin@viacesi.fr>
2022-03-22 18:16:23 +0100

    ajout du css sur internship proposal

--- internship_proposal.php
+++ internship_proposal.php
@@ -3,30 +3,30 @@
     <?php require "head.php" ?>
     <title> Create an internship proposal </title>
     <meta charset="description" content="Page where you create internship proposal">
-    <link rel="stylesheet" href="internship_proposal.scss">
+    <link rel="stylesheet" href="./scss/internship_proposal.scss">
     </head>
 
 <body>
         <?php require "header-stu.php"?>
         <main>
                 <h1>Create an Internship Proposal</h1>
-
-                <td><label>What entreprise ?</label></td>
+                <label for="What entreprise ?">What company ?</label>
                 <br>
-                <label for="What entreprise ?"> <em>*</em></label>
-                <input id="nom" placeholder="CESI" autofocus="" required="">
-                <td><label>Which domain ?</label></td>
+                <input type="text" id="company" placeholder="CESI" autofocus="" required="">
                 <br>
-                <label for="Which domain ?"> <em>*</em></label>
-                <input id="nom" placeholder="Domain" autofocus="" required="">
-                <td><label>Profile</label></td>
+                <table>
+                        <td><label for="Which domain ?">Which domain ?</label></td>
+                        <td><label for="Profile">Profile</label></td>
+                        <tr>
+                        <td><input type="text" id="domain" placeholder="Domain" required=""></td>
+                        <td><input type="text" id="profile" placeholder="Profile" required=""></td>
+                </table>
                 <br>
-                <label for="Profile"> <em>*</em></label>
-                <input id="nom" placeholder="Profile" autofocus="" required="">
                 <label for="Description">Description</label>
                 <br>
-                <input type="text" id="Description">
-                <input type="submit" value="Submit">
+                <textarea  id="Description"></textarea>
+                <br>
+                <input type="submit" value="Submit" id="sub">
                 </form>
         </main> 
         <?php require "footer.php"?>
