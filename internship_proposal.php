f315b9693210303205c0ed5f0357fa4822555f27
SwenA <swen.aubertin@viacesi.fr>
2022-03-22 15:49:11 +0100

    fichier sass créé pour internship proposal

--- internship_proposal.php
+++ internship_proposal.php
@@ -3,32 +3,31 @@
     <?php require "head.php" ?>
     <title> Create an internship proposal </title>
     <meta charset="description" content="Page where you create internship proposal">
-    <link rel="stylesheet" href="internship-proposal.css">
+    <link rel="stylesheet" href="internship_proposal.scss">
     </head>
 
 <body>
         <?php require "header-stu.php"?>
-        <h1>Create an Internship Proposal</h1>
-    
-        <td><label>What entreprise ?</label></td>
-        <br>
-        <label for="What entreprise ?"> <em>*</em></label>
-        <input id="nom" placeholder="CESI" autofocus="" required=""><br>
-<br>
-        <td><label>Which domain ?</label></td>
-        <br>
-        <label for="Which domain ?"> <em>*</em></label>
-        <input id="nom" placeholder="Domain" autofocus="" required=""><br>
-<br>
-        <td><label>Profile</label></td>
-        <br>
-        <label for="Profile"> <em>*</em></label>
-        <input id="nom" placeholder="Profile" autofocus="" required=""><br>
-<br>
-        <label for="Description">Description</label><br>
-        <textarea id="Description"></textarea>
-<br><br>
-        <input type="submit" value="Submit">
-        </form>
+        <main>
+                <h1>Create an Internship Proposal</h1>
+
+                <td><label>What entreprise ?</label></td>
+                <br>
+                <label for="What entreprise ?"> <em>*</em></label>
+                <input id="nom" placeholder="CESI" autofocus="" required="">
+                <td><label>Which domain ?</label></td>
+                <br>
+                <label for="Which domain ?"> <em>*</em></label>
+                <input id="nom" placeholder="Domain" autofocus="" required="">
+                <td><label>Profile</label></td>
+                <br>
+                <label for="Profile"> <em>*</em></label>
+                <input id="nom" placeholder="Profile" autofocus="" required="">
+                <label for="Description">Description</label>
+                <br>
+                <input type="text" id="Description">
+                <input type="submit" value="Submit">
+                </form>
+        </main> 
         <?php require "footer.php"?>
 </body>
