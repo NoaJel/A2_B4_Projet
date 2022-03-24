8e9357f42f96be56379d54485bca848b050b1845
SwenA <swen.aubertin@viacesi.fr>
2022-03-24 20:47:04 +0100

    ajout de company management avec style

--- company-management.php
+++ company-management.php
@@ -6,22 +6,38 @@
 <body>
     <?php require_once "header-stu.php" ?>
     <main>
-        <h1>Company Management</h1>
-        <p class="flotte">
-            <img src="./images/logo.png" 
-            width="150"/> </p>
-
         <form action="" method="POST">
-            <label>Name</label>
-            <input type="text">
-            <label>Domain of activity</label>
-            <input type="text">
-            <label>Location</label>
-            <input type="text">
-            <label>Add tags</label>
-            <input type="text">
-            <label>Description</label>
-            <textarea></textarea>
+            <div class="parent1">
+                <legend><h1>Company Management</h1></legend>
+                <div class="form-img">
+                    <input type="file">
+                </div>
+            </div>
+            <div class="parent2">
+                <div class="form-field">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" placeholder="Your company name" required>
+                </div>
+                <div class="form-field">
+                    <label for="domain">Domain of activity</label>
+                    <input type="text" id="domain" placeholder="Domains (e.g. web dev, pentesting, etc.)" required>
+                </div>
+                <div class="form-field">
+                    <label for="location">Location</label>
+                    <input type="text" id="location" placeholder="where you are located" required>
+                </div>
+                <div class="form-field">
+                    <label for="tags">Add tags</label>
+                    <input type="text" id="tags" placeholder="add some tags to precise your profile (e.g. #devweb, #java, etc.)" required>
+                </div>
+                <div class="form-field">
+                    <label for="description">Description</label>
+                    <textarea id="description" placeholder="Put more details here about your company"></textarea>
+                </div>
+                <div class="form-submit">
+                    <input type="submit" value="Validate →">
+                </div>
+            </div>
         </form>
     </main>
     <?php require_once "footer.php" ?>
