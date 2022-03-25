d9d4bae8753a5ac72085afd342169028ebba2e22
NoaJel <noa.jelsch@viacesi.fr>
2022-03-25 02:25:13 +0100

    scss architecture changed: using 7-1 architecture instead of nothing

--- head.php
+++ head.php
@@ -5,9 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="./scss/header-stu.css" type="text/css" />
-    <!-- <link rel="stylesheet" href="./scss/header-adm.css" type="text/css" /> -->
-    <link rel="stylesheet" href="./scss/footer.css" type="text/css" />
+    <link rel="stylesheet" href="./scss/main.css" type="text/css" />
     <!-- FONTS -->
     <link rel="stylesheet" href="./fonts/fonts.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
