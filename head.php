1a00858796134dfd0107c6cdb15d394b6e312fc2
SwenA <swen.aubertin@viacesi.fr>
2022-03-28 10:29:57 +0200

    ajout du des fichiers du main

--- head.php
+++ head.php
@@ -7,12 +7,15 @@
     <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="./scss/main.css" type="text/css" />
     <!-- FONTS -->
-    <link rel="stylesheet" href="./fonts/fonts.css" />
+    <link rel="preload" as="style" href="./fonts/fonts.css" onload="this.rel='stylesheet'" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500;700&display=swap" />
+    <!-- <link rel="preload" as="font" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500;700&display=swap" crossorigin="" /> -->
     <!-- /FONTS -->
     <!-- FONTAWESOME -->
-    <link rel="stylesheet" href="./fontawesome-free-5.15.4-web/css/all.min.css" />
+    <link rel="preload" as="style" href="./fontawesome-free-5.15.4-web/css/all.min.css" onload="this.rel='stylesheet'" />
+    <!-- <link rel="stylesheet" href="./fontawesome-free-5.15.4-web/css/all.min.css" /> -->
     <!-- /FONTAWESOME -->
-    <script src="./js/header-stu.js" defer></script>
+    <script type="text/javascript" src="./jquery/jquery-3.6.0.min.js" defer></script>
+    <script type="text/javascript" src="./js/header-stu.js" defer></script>
